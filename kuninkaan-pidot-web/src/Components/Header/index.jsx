eb7d50fa7ef0d563c05e42349e31245e734163d4
Sampsoni <sami.repo@live.com>
2018-02-07 13:33:40 +0200

    Added ClassNames into npm file

--- kuninkaan-pidot-web/src/Components/Header/index.jsx
+++ kuninkaan-pidot-web/src/Components/Header/index.jsx
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <header id="header">
-        <div className="logo"><a href="index.html">Kuninkaan Pidot</a></div>
+        <div className="logo"><a href="/">Kuninkaan Pidot</a></div>
 				<a href="#menu"><span>Menu</span></a>
       </header>
     );
